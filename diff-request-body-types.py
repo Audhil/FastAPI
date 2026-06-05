c2ded132f9c24ba5d6746c269000acfc59fdff7d
Shakeer Mohammed Audhil <audhil.mohammedaudhil@maybank.com>
2026-06-05 23:48:53 +0800

    add diff request body types sample

--- diff-request-body-types.py
+++ diff-request-body-types.py
@@ -1,5 +1,5 @@
 # request body types = json, text, form, file
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Form, UploadFile, File
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -11,6 +11,7 @@ class Item(BaseModel):
     in_stock: bool
 
 
+# json
 @app.post("/json")
 def receive_json(item: Item):
     return {"type": "JSON",
@@ -19,7 +20,24 @@ def receive_json(item: Item):
             "price": item.price}
 
 
+# text
 @app.post("/text")
 def receive_text(content: str = Body(..., media_type="text/plain")):  # ... means mandatory; None means optional
     return {"type": "Plain text",
             "content": content}
+
+
+# form
+@app.post("/form")
+def receive_form(username: str = Form(...), password: str = Form(...)):
+    return {"type": "form",
+            "username": username,
+            "password": password}
+
+
+# file upload
+@app.post("/upload")
+def receive_upload_file(file: UploadFile = File(...)):
+    return {"type": "file",
+            "filename": file.filename,
+            "content_type": file.content_type}
